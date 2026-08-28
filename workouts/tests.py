003083b8f7c702a30b901d7c73d2ea0911d348cd
uk6655 <k.umemoto06252@gmail.com>
2026-08-28 17:29:52 +0900

    Add login tests

--- workouts/tests.py
+++ workouts/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="testpassword"
+        )
+
+    def test_login(self):
+        response = self.client.post(
+            reverse("login"),
+            {
+                "username": "testuser",
+                "password": "testpassword",
+            }
+        )
+
+        self.assertRedirects(
+            response,
+            reverse("home")
+        )
+
+    def test_home_requires_login(self):
+        response = self.client.get(
+            reverse("home")
+        )
+
+        self.assertRedirects(
+            response,
+            f"{reverse('login')}?next={reverse('home')}"
+        )
+
+    def test_home_after_login(self):
+        self.client.login(
+            username="testuser",
+            password="testpassword"
+        )
+
+        response = self.client.get(
+            reverse("home")
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
